3da7dccf23e7033be625a7c5770fd6b252606c08
kellyofo <keofordile@gmail.com>
2024-07-23 13:08:16 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Apollo"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Prometheus"
+    chat_description: str = "Prometheus is a chatbot configured to answer your questions on Apollo procedures"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
